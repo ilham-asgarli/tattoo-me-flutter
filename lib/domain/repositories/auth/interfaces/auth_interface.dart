d99ef35f9594f0e954f1a92d6ce87577160c78db
ilham-asgarli <ilhamasgarli19@gmail.com>
2022-11-30 04:15:03 +0300

    Initial commit

--- lib/domain/repositories/auth/interfaces/auth_interface.dart
+++ lib/domain/repositories/auth/interfaces/auth_interface.dart
@@ -2,6 +2,5 @@ import '../../../../core/base/models/base_response.dart';
 
 abstract class AuthInterface {
   Future<BaseResponse> getCurrentUser();
-  Future<BaseResponse> getNotAnonymousCurrentUser();
   Future<BaseResponse> signOut();
 }
