7407767e28f4f027549a18fc093029b1144314ff
ilham-asgarli <ilhamasgarli19@gmail.com>
2023-05-04 16:17:41 +0300

    Initial commit

--- scripts/push.cmd
+++ scripts/push.cmd
@@ -1,5 +1,5 @@
 git add .
 set /p "message=Message: "
-if not defined %message% ( set "message=Initial commit" )
+if [%message%]==[] ( set "message=Initial commit" )
 git commit -m "%message%"
 git push -u origin main
